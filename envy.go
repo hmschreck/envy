b323c021f319937a790460a41c2922f02140aeaa
Harold Schreckengost <harold@haroldmschreckengost.com>
2020-03-09 11:27:32 -0500

    Added GetEnvInt

--- envy.go
+++ envy.go
@@ -1,10 +1,21 @@
 package envy
 
-import "os"
+import (
+	"os"
+	"strconv"
+)
 
 func GetEnv(env_key, fallback string) (env string) {
 	if value, ok := os.LookupEnv(env_key); ok {
 		return value
 	}
 	return fallback
+}
+
+func GetEnvInt(env_key, fallback string) (env int, err error) {
+	env, err = strconv.Atoi(GetEnv(env_key, fallback))
+	if err != nil {
+		return
+	}
+	return
 }
