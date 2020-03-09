630c0bd18f32c03781278e89cea611c9388017c4
Harold Schreckengost <harold@haroldmschreckengost.com>
2020-03-09 11:35:11 -0500

    Tests!

--- envy_test.go
+++ envy_test.go
@@ -3,6 +3,7 @@ package envy
 import (
 	"os"
 	"testing"
+	"github.com/magiconair/properties/assert"
 )
 
 func TestGetEnvNoEnv(t *testing.T) {
@@ -20,5 +21,29 @@ func TestGetEnvWithEnv(t *testing.T) {
 	result := GetEnv("TEST_KEY_X", "FALLBACK")
 	if result != "ENV" {
 		t.Error("Expected ENV received", result)
+	}
+}
+
+func TestGetEnvInt(t *testing.T) {
+	os.Setenv("TEST_INT", "24")
+	result, err := GetEnvInt("TEST_INT", "23")
+	if err != nil {
+		t.Error("GOt error when shouldn't have")
+	}
+	assert.Equal(t, result, 24)
+}
+
+func TestGetEnvIntFallback(t *testing.T) {
+	result, err := GetEnvInt("TEST_INT_2", "23")
+	if err != nil {
+		t.Error("Got error when shouldn't have")
+	}
+	assert.Equal(t, result, 23)
+}
+
+func TestGetEnvIntNonInt(t *testing.T) {
+	_, err := GetEnvInt("TEST_INT_NONE", "asdf")
+	if err == nil {
+		t.Error("Did not receive error when should have")
 	}
 }
